ddd51095f79a5c592eed7ba584e89353b36e9917
ashu2108 <ashutosh.teknur99@gmail.com>
2019-06-10 07:50:22 +0000

    added files

---
+++ .bash_history
@@ -0,0 +1,10 @@
+ls
+vi /etc/hostname
+hostname 
+hostname master
+ls
+cd role
+ls
+cd roles
+ls
+vi script.yml 
---
+++ ansible.py
@@ -0,0 +1,10 @@
+import os
+import sys
+os.system(" sudo apt update -y ")
+os.system(" sudo apt install software-properties-common -y ")
+os.system(" sudo apt-add-repository ppa:ansible/ansible -y ")
+os.system(" sudo apt update -y ")
+os.system(" sudo apt install ansible -y ")
+os.system(" clear ")
+os.system(" sudo ansible --version ")
+
